bd84922a64497666927080e4bc802affa773db06
Ahmad Zaky <a.zaky32@gmail.com>
2021-09-09 09:46:43 +0700

    Tidying up the code

--- 01.js
+++ 01.js
@@ -1,28 +1,27 @@
 const biodata = {
-    name: "Ahmad Zaky",
-    age: 26,
-    hobbies: ["Sepak Bola", "Gaming", "Anime", "IT"],
-    isMarried: false,
-    schoolList: [
-      { name: "SD Muhammadiyah 5", yearIn: "2001", yearOut: "2007", Major: null },
-      { name: "SMP Negeri 6", yearIn: "2007", yearOut: "2010", Major: null },
-      { name: "SMA Bina Warga 2", yearIn: "2010", yearOut: "2013", Major: "IPS" },
-      {
-        name: "Universitas Sriwijaya",
-        yearIn: "2013",
-        yearOut: "2017",
-        Major: "Akuntansi",
-      },
-    ],
-    skills: [
-      { skillName: "Microsoft Office", Level: "advanced" },
-      { skillName: "Javascript", Level: "beginner" },
-      { skillName: "HTML", Level: "beginner" },
-      { skillName: "CSS", Level: "beginner" },
-      { skillName: "Nodejs", Level: "beginner" },
-      { skillName: "Reactjs", Level: "beginner" },
-    ],
-    interestInCoding: true,
-  };
-  console.log(biodata);
-  
+  name: "Ahmad Zaky",
+  age: 26,
+  hobbies: ["Sepak Bola", "Gaming", "Anime", "IT"],
+  isMarried: false,
+  schoolList: [
+    { name: "SD Muhammadiyah 5", yearIn: "2001", yearOut: "2007", Major: null },
+    { name: "SMP Negeri 6", yearIn: "2007", yearOut: "2010", Major: null },
+    { name: "SMA Bina Warga 2", yearIn: "2010", yearOut: "2013", Major: "IPS" },
+    {
+      name: "Universitas Sriwijaya",
+      yearIn: "2013",
+      yearOut: "2017",
+      Major: "Akuntansi",
+    },
+  ],
+  skills: [
+    { skillName: "Microsoft Office", Level: "advanced" },
+    { skillName: "Javascript", Level: "beginner" },
+    { skillName: "HTML", Level: "beginner" },
+    { skillName: "CSS", Level: "beginner" },
+    { skillName: "Nodejs", Level: "beginner" },
+    { skillName: "Reactjs", Level: "beginner" },
+  ],
+  interestInCoding: true,
+};
+console.log(biodata);
--- 03.js
+++ 03.js
@@ -1,14 +1,14 @@
 const printSegitiga = 5;
 
 if (typeof printSegitiga === "number") {
-	for (i = printSegitiga; i >= 1; i--) {
-		let print = "";
-	
-		for (j = 1; j <= i; j++) {
-			print += j;
-		}
-		console.log(print);
-	}
+  for (i = printSegitiga; i >= 1; i--) {
+    let print = "";
+
+    for (j = 1; j <= i; j++) {
+      print += j;
+    }
+    console.log(print);
+  }
 } else {
-	console.log("Data harus number");
+  console.log("Data harus number");
 }
